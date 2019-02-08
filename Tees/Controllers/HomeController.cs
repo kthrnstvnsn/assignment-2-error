67082f16477f129ab32bee1dcb84e44c36bd7ff7
kthrnstvnsn <katherine.stevenson1@gmail.com>
2019-02-08 15:16:15 +1300

    up to the end of chapter 17

--- Tees/Controllers/HomeController.cs
+++ Tees/Controllers/HomeController.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Tees.DAL;
+using Tees.ViewModels;
 
 namespace Tees.Controllers
 {
     public class HomeController : Controller
     {
+        private StoreContext db = new StoreContext();
         public ActionResult Index()
         {
-            return View();
+            var topSellers = (from topProducts in db.OrderLines
+                              where (topProducts.ProductID != null)
+                              group topProducts by topProducts.Product into topGroup
+                              select new BestSellersViewModel
+                              {
+                                  Product = topGroup.Key,
+                                  SalesCount = topGroup.Sum(o => o.Quantity),
+                                  ProductImage = topGroup.Key.ProductImageMappings.
+                              OrderBy(pim
+                              =>
+                              pim.ImageNumber).FirstOrDefault().ProductImage.FileName
+                              }).OrderByDescending(tg => tg.SalesCount).Take(3);
+            return View(topSellers);
         }
 
         public ActionResult About(string id)
